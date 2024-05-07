0da947b81398b9469723ea1126d2674deebde739
Bhavya <bgprajapati575@gmail.com>
2024-05-07 17:28:26 +0530

    Final Commit

--- MentorPedia/index.js
+++ MentorPedia/index.js
@@ -4,13 +4,19 @@ const bodyParser = require('body-parser');
 const port = 3001;
 const app = express();
 
+// var client = new SolrNode({
+//   host: 'localhost',
+//   port: '8983',
+//   core: 'bhavya',
+//   protocol: 'http'
+// });
+
 var client = new SolrNode({
   host: 'localhost',
   port: '8983',
-  core: 'data2',
+  core: 'bhavya1',
   protocol: 'http'
 });
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -21,10 +27,14 @@ app.post('/getProduct', function (req, res) {
 
   const searchTerm = req.body.name;
   console.log('Search Term:', searchTerm);
-  const strQuery = client.query()
-    .q("name:" + searchTerm + "* OR college:" + searchTerm + "* OR dept:" + searchTerm + "* OR email:" + searchTerm + "* OR research_areas:" + searchTerm + "* OR website:" + searchTerm + "*")
-    .sort('experience', 'asc');
+  // const strQuery = client.query()
+  // .q("name:" + searchTerm + "* OR college:" + searchTerm + "* OR dept:" + searchTerm + "* OR email:" + searchTerm + "* OR research_areas:" + searchTerm + "* OR website:" + searchTerm + "*")
+  // .sort('experience', 'asc')
+  // .rows(100);
 
+  const strQuery = client.query()
+  .q("name:" + searchTerm + "* OR position:" + searchTerm + "* OR ph.d:" + searchTerm + "* OR department_s_:" + searchTerm + "* OR research_interests:" + searchTerm + "* OR office_address:" + searchTerm + "* OR office_phone:" + searchTerm + "*")
+  .sort('position', 'asc').rows(100);
 
   client.search(strQuery, function (err, result) {
     if (err) {
